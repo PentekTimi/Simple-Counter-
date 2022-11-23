ceb5f071a306eade1a67be5fe5dcf9036c7b48dc
Timea <pentektimea98@gmail.com>
2022-11-23 16:55:10 +0000

    Add Counter Files

---
+++ script.js
@@ -0,0 +1,25 @@
+const num = document.getElementById('value')
+const btns = document.querySelectorAll(".btn");
+
+let count = 0;
+
+
+btns.forEach((btn) => {
+	btn.addEventListener("click", (e) => {
+		const styles = e.currentTarget.classList;
+
+			if (styles.contains("increase")) {
+				count++
+			}
+			else if(styles.contains("decrease")) {
+				count--
+			}
+			else {
+				count = 0;
+			}
+		num.textContent = count;
+	});
+});
+
+
+
